a9ea9e29bde1b8ef66a60e8673c70997cbdc8695
DarkDev <darkdev_brand@riseup.net>
2022-10-01 01:25:14 +0300

    add proto bio section

---
+++ src/components/bio/WhatWeDoCard.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Card, Button} from "flowbite-svelte";
+
+  export let heading, description, reverse, img;
+</script>
+
+<div class="md:w-3/12 ">
+  <Card class="m-2 " img={img} reverse={reverse}>
+    <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{heading}</h5>
+    <p class="mb-2 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+      {description}
+    </p>
+    <!-- <Button>
+      Read more <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg>
+    </Button> -->
+  </Card>
+</div>
