4e32022023dc82a2d8c7b6b6dc5cd519ff3c39c6
DarkDev <darkdev_brand@riseup.net>
2022-10-01 01:26:12 +0300

    add proto boardgames section

---
+++ src/components/boardgames/BoardGamesItem.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { AccordionItem} from 'flowbite-svelte';
+  
+
+  export let name, description, link, images;
+</script>
+
+
+
+
+  
---
+++ src/components/boardgames/BoardgamesCategory.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Accordion, AccordionItem } from 'flowbite-svelte';
+import BoardgamesItem from './BoardGamesItem.svelte';
+import { Img } from 'flowbite-svelte';
+
+//   export let categoryList;
+</script>
+
+
+<Accordion
+  activeClasses="bg-blue-100 dark:bg-gray-800 text-blue-600 dark:text-white focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800"
+  inactiveClasses="text-gray-500 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800">
+  <AccordionItem>
+    <h2 slot="header">Header 2-1</h2>
+    <div slot="body">
+      <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
+    </div>
+  </AccordionItem>
+  <AccordionItem>
+    <h2 slot="header">Header 2-2</h2>
+    <div slot="body">
+      <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
+    </div>
+  </AccordionItem>
+</Accordion>
