a4999e4c70f942753bbc95463455d85ac5a7217e
DarkDev <darkdev_brand@riseup.net>
2022-10-01 01:26:43 +0300

    banner proto

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { CarouselTransition } from 'flowbite-svelte'
+//   import { images } from './imageData/+server.js';
+  import { bounceInOut } from 'svelte/easing';
+
+
+</script>
+    
+<section class="">
+
+</section>
+
+<style> 
+
+section {
+  /* Location of the image */
+  background-image: url(https://raw.githubusercontent.com/oleg-darkdev/44hackerspace/dev/static/social/banner.png);
+  width: 100%;
+  height: 500px;
+  
+  /* Image is centered vertically and horizontally at all times */
+  background-position: center center;
+  
+  /* Image doesn't repeat */
+  background-repeat: no-repeat;
+  
+  /* Makes the image fixed in the viewport so that it doesn't move when 
+     the content height is greater than the image height */
+  background-attachment: fixed;
+  
+  /* This is what makes the background image rescale based on its container's size */
+  background-size: cover;
+  
+  /* Pick a solid background color that will be displayed while the background image is loading */
+  background-color:#464646;
+  
+  /* SHORTHAND CSS NOTATION
+   * background: url(background-photo.jpg) center center cover no-repeat fixed;
+   */
+}
+
+/* For mobile devices */
+@media only screen and (max-width: 767px) {
+  body {
+    /* The file size of this background image is 93% smaller
+     * to improve page load speed on mobile internet connections */
+    background-image: url(https://www.amateurphotographer.co.uk/wp-content/uploads/2020/09/Quiraing-1115-scaled.jpg);
+  }
+}
+
+</style>
