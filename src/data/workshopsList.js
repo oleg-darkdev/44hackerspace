68ab7ac5cc55332925a3ad242473e9dfa6fa5bb7
DarkDev <darkdev_brand@riseup.net>
2022-12-08 18:08:23 +0200

    refactoring

---
+++ src/data/workshopsList.js
@@ -0,0 +1,254 @@
+const workshopsList = [
+	{
+		title: 'Turnieje szachowe',
+		description: [
+			'Zapraszamy do udziału w turnieju szachów klasycznych "Żółty słoń", rozgrywany zgodnie z systemem szwajcarskim. ',
+			'Sędzia: kandydat na mistrza sportu - Oleg Miedwiediew. Mistrz Republiki Białorusi 2005-2006, wielokrotny mistrz międzynarodowych turniejów szachowych. ',
+			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+		],
+		img: '/workshops/workshop_chess.png',
+		partners: [
+			{
+				logo: '/partners/partner_44_cubes.svg',
+				link: '',
+				title: '44 cubes'
+			},
+			{
+				logo: '/partners/partner_darkdev.svg',
+				link: '',
+				title: 'DarkDev progect'
+			},
+			{
+				logo: '/partners/partner_itingo.svg',
+				link: '',
+				title: 'ITingo'
+			},
+			{
+				logo: '/partners/partner_elephant.svg',
+				link: '',
+				title: 'Yellow Elephant'
+			}
+		],
+		harmonogram: [
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			},
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			},
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			}
+		]
+	},
+	{
+		title: 'Naprawa rowerów w hackerspace',
+		description: [
+			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+		],
+		img: '/workshops/workshop_bicycle.png',
+		partners: [
+			{
+				logo: '/partners/partner_44_cubes.svg',
+				link: '',
+				title: '44 cubes'
+			},
+			{
+				logo: '/partners/partner_darkdev.svg',
+				link: '',
+				title: 'DarkDev progect'
+			},
+			{
+				logo: '/partners/partner_itingo.svg',
+				link: '',
+				title: 'ITingo'
+			}
+		],
+		harmonogram: [
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			},
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			},
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			}
+		]
+	},
+	{
+		title: 'Zagrajmy razem w gry planszowe',
+		description: [
+			'Gry planszowe to okazja do intelektualnej zabawy i miłego spędzenia wieczoru. Zapraszamy wszystkich, którzy tęsknią za grami planszowymi i tych, którzy będą grać pierwszy raz.',
+			'Możesz przynieść swoje ulubione gry i zawsze znajdziesz kogoś chętnego do zagrania z Tobą.'
+		],
+		img: '/workshops/workshop_boardgames.png',
+		partners: [
+			{
+				logo: '/partners/partner_44_cubes.svg',
+				link: '',
+				title: '44 cubes'
+			},
+			{
+				logo: '/partners/partner_darkdev.svg',
+				link: '',
+				title: 'DarkDev progect'
+			},
+			{
+				logo: '/partners/partner_itingo.svg',
+				link: '',
+				title: 'ITingo'
+			}
+		],
+		harmonogram: [
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			},
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			},
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			}
+		]
+	},
+	{
+		title: 'Naprawa domowych urządzeń elektrycznych',
+		description: [
+			'Dajemy urządzeniom drugie życie.',
+			'Naucz się naprawiać zepsuty sprzęt AGD: czajniki, przedłużacze, urządzenia z przerwanymi przewodami. Podczas warsztatu nauczysz się: prawidłowo lutować przewody, rozkręcać  urządzenia elektryczne, jak używać multimetru.',
+			'Zapraszamy do udziału, można przynieść własne urządzenia elektryczne.'
+		],
+		img: '/workshops/workshop_electric.png',
+		partners: [
+			{
+				logo: '/partners/partner_44_cubes.svg',
+				link: '',
+				title: '44 cubes'
+			},
+			{
+				logo: '/partners/partner_darkdev.svg',
+				link: '',
+				title: 'DarkDev progect'
+			},
+			{
+				logo: '/partners/partner_itingo.svg',
+				link: '',
+				title: 'ITingo'
+			}
+		],
+		harmonogram: [
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			},
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			},
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			}
+		]
+	},
+	{
+		title: 'Ręcznie wyrabiane gier planszowych',
+		description: [
+			'Masz pomysł na własną grę planszową ? Przyjdź na warsztat, na którym pokażemy Ci technologię tworzenia gier planszowych - własnymi rękami.',
+			'Możecie przynieść swoje wydrukowane gry planszowe, a my pomożemy Wam je zgrabnie wyciąć lub przenieść na karton.'
+		],
+		img: '/workshops/workshop_boardgames_diy.png',
+		partners: [
+			{
+				logo: '/partners/partner_44_cubes.svg',
+				link: '',
+				title: '44 cubes'
+			},
+			{
+				logo: '/partners/partner_darkdev.svg',
+				link: '',
+				title: 'DarkDev progect'
+			},
+			{
+				logo: '/partners/partner_itingo.svg',
+				link: '',
+				title: 'ITingo'
+			}
+		],
+		harmonogram: [
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			},
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			},
+			{
+				date: '22.02.2022',
+				linkFb: '',
+				linkTg: '',
+				img: '',
+				title: 'Neil Sims'
+			}
+		]
+	}
+];
+
+
+
+export default workshopsList;
