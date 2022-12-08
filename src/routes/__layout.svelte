22727a5ee1c6bb4f8357d1091b4c60b597b0ba21
DarkDev <darkdev_brand@riseup.net>
2022-12-08 18:08:42 +0200

    clear, add global style for social icons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,12 +2,16 @@
 	import '../app.css';
 	import Header from '../components/Header.svelte';
 	// import Footer from '../components/Footer.svelte';
-
 </script>
+
 <Header />
 
 <slot />
 
-
-<!-- <DarkMode /> -->
 <!-- <Footer /> -->
+
+<style>
+	:global(.social-hover:hover) {
+	  background-color: #F3E600;
+	}
+</style>
