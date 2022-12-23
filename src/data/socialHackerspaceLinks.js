3a5d2834ddfa0f501ecdc4461976f712f13c0a55
DarkDev <darkdev_brand@riseup.net>
2022-12-23 15:56:57 +0200

    clear social

--- src/data/socialHackerspaceLinks.js
+++ src/data/socialHackerspaceLinks.js
@@ -1,67 +1,61 @@
 const socialLinks = {
-	instagram: {
-		name: 'Instagram',
-		logoImg: '',
-		link: 'https://www.instagram.com/44hackerspace/',
-		logoSvg: ''
-	},
+	// instagram: {
+	// 	name: 'Instagram',
+	// 	
+	// 	link: 'https://www.instagram.com/44hackerspace/',
+	// 	
+	// },
 	facebook: {
 		name: 'Facebook',
-		logoImg: '',
+		
 		link: 'https://www.facebook.com/44hackerspace',
-		logoSvg: ''
+		
 	},
 	telegram: {
 		name: 'Telegram',
-		logoImg: '',
+		
 		link: 'https://t.me/hackerspace44',
-		logoSvg: ''
+		
 	},
-	twitter: {
-		name: 'Twitter',
-		logoImg: '',
-		link: 'https://twitter.com/44hackerspace',
-		logoSvg: ''
-	},
+	// twitter: {
+	// 	name: 'Twitter',
+	// 	
+	// 	link: 'https://twitter.com/44hackerspace',
+	// 	
+	// },
 	github: {
 		name: 'Github',
-		logoImg: '',
+		
 		link: 'https://github.com/44hackerspace',
-		logoSvg: ''
+		
 	},
 	linkedin: {
 		name: 'Linkedin',
-		logoImg: '',
+		
 		link: '',
-		logoSvg: ''
+		
 	},
 	patronite: {
 		name: 'Patronite',
-		logoImg: '',
+		
 		link: '',
-		logoSvg: ''
+		
 	},
-	etsy: {
-		name: 'Etsy',
-		logoImg: '',
-		link: '',
-		logoSvg: ''
-	},
-	eventBrite: {
-		name: 'Event brite',
-		logoImg: '',
-		link: '',
-		logoSvg: ''
-	},
-	meetup: {
-		name: 'Meetup',
-		logoImg: '',
-		link: '',
-		logoSvg: ''
-	}
+	// etsy: {
+	// 	name: 'Etsy',
+	// 	
+	// 	link: '',
+	// 	
+	// },
+	// meetup: {
+	// 	name: 'Meetup',
+	// 	
+	// 	link: '',
+	// 	
+	// }
 };
 
 
 
 
-export default socialLinks
+export default socialLinks;
