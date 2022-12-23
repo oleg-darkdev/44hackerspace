becc7c3343a56282f18fa0865aabdba58fbe4f85
DarkDev <darkdev_brand@riseup.net>
2022-12-23 15:57:12 +0200

    add links to workshops on fb

--- src/data/workshopsList.js
+++ src/data/workshopsList.js
@@ -31,25 +31,11 @@ const workshopsList = [
 		],
 		harmonogram: [
 			{
-				date: '22.02.2022',
-				linkFb: '',
+				date: '17.11.2022',
+				linkFb: 'https://www.facebook.com/events/3222760484644932',
 				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
-			},
-			{
-				date: '22.02.2022',
-				linkFb: '',
-				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
-			},
-			{
-				date: '22.02.2022',
-				linkFb: '',
-				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
+				img: 'https://scontent-frt3-1.xx.fbcdn.net/v/t39.30808-6/312622489_1324585104612954_3132663826861128700_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=340051&_nc_ohc=WsEjQc2LJJ8AX8IKJOI&_nc_ht=scontent-frt3-1.xx&oh=00_AfCslML0jJGwWmQHW56v1tELm8o7OkJGB8UuHyWcAWSCUQ&oe=63AB009F',
+				title: 'Turnieje szachowy #1'
 			}
 		]
 	},
@@ -80,25 +66,48 @@ const workshopsList = [
 		],
 		harmonogram: [
 			{
-				date: '22.02.2022',
-				linkFb: '',
+				date: '15.12.2022',
+				linkFb: 'https://www.facebook.com/events/680320676921466',
 				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
+				img: 'https://scontent-frt3-1.xx.fbcdn.net/v/t39.30808-6/318298610_1348721775532620_4007767599026375955_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=340051&_nc_ohc=kw4jVaDmP58AX-ziP0p&_nc_ht=scontent-frt3-1.xx&oh=00_AfBoQwDxiqrYy657ie_2c8DXpmbkwc0HDo6_ZsJkdGrI-Q&oe=63AB85A2',
+				title: 'Naprawa rowerów #6'
 			},
 			{
-				date: '22.02.2022',
-				linkFb: '',
+				date: '22.11.2022',
+				linkFb: 'https://www.facebook.com/events/460124089546642',
 				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
+				img: 'https://scontent-frx5-1.xx.fbcdn.net/v/t39.30808-6/311941674_1324588271279304_494726408658916893_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=340051&_nc_ohc=kqVz7YXkV74AX9bRLfk&_nc_ht=scontent-frx5-1.xx&oh=00_AfBHnVCKhZA9LqqsqqaZp2ka5J1sFvz-PZ5u_aPEqh2Y5w&oe=63AB9917',
+				title: 'Naprawa rowerów #5'
 			},
 			{
-				date: '22.02.2022',
-				linkFb: '',
+				date: '18.10.2022',
+				linkFb: 'https://www.facebook.com/events/1233578787488137',
+				linkTg: '',
+				img: 'https://scontent-frx5-1.xx.fbcdn.net/v/t39.30808-6/310125923_1302403406831124_2936998602256963597_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=340051&_nc_ohc=ORss4jyMpvEAX9HdgTu&_nc_ht=scontent-frx5-1.xx&oh=00_AfDjASHe-6FR69yqfe96Yfwx7rNM3vNXPJfkiHzvpSymdQ&oe=63AB99C1',
+				title: 'Naprawa rowerów #4'
+			},
+			// upd img
+			{
+				date: '20.09.2022',
+				linkFb: 'https://www.facebook.com/events/1193753578150133',
+				linkTg: '',
+				img: 'https://scontent-frx5-1.xx.fbcdn.net/v/t39.30808-6/310125923_1302403406831124_2936998602256963597_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=340051&_nc_ohc=ORss4jyMpvEAX9HdgTu&_nc_ht=scontent-frx5-1.xx&oh=00_AfDjASHe-6FR69yqfe96Yfwx7rNM3vNXPJfkiHzvpSymdQ&oe=63AB99C1',
+				title: 'Naprawa rowerów #3'
+			},
+			// upd img
+			{
+				date: '06.09.2022',
+				linkFb: 'https://www.facebook.com/events/486487283321683',
+				linkTg: '',
+				img: 'https://scontent-frx5-1.xx.fbcdn.net/v/t39.30808-6/310125923_1302403406831124_2936998602256963597_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=340051&_nc_ohc=ORss4jyMpvEAX9HdgTu&_nc_ht=scontent-frx5-1.xx&oh=00_AfDjASHe-6FR69yqfe96Yfwx7rNM3vNXPJfkiHzvpSymdQ&oe=63AB99C1',
+				title: 'Naprawa rowerów #2'
+			},
+			{
+				date: '21.07.2022',
+				linkFb: 'https://www.facebook.com/events/594389005594767',
 				linkTg: '',
 				img: '',
-				title: 'Neil Sims'
+				title: 'Naprawa rowerów #1'
 			}
 		]
 	},
@@ -128,25 +137,55 @@ const workshopsList = [
 		],
 		harmonogram: [
 			{
-				date: '22.02.2022',
-				linkFb: '',
+				date: '08.12.2022',
+				linkFb: 'https://www.facebook.com/events/704342584282178',
 				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
+				img: 'https://scontent-frt3-1.xx.fbcdn.net/v/t39.30808-6/318298610_1348721775532620_4007767599026375955_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=340051&_nc_ohc=kw4jVaDmP58AX-ziP0p&_nc_ht=scontent-frt3-1.xx&oh=00_AfBoQwDxiqrYy657ie_2c8DXpmbkwc0HDo6_ZsJkdGrI-Q&oe=63AB85A2',
+				title: 'Zagrajmy razem w gry planszowe #7'
 			},
 			{
-				date: '22.02.2022',
-				linkFb: '',
+				date: '17.11.2022',
+				linkFb: 'https://www.facebook.com/events/497548852283980',
 				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
+				img: 'https://scontent-frx5-1.xx.fbcdn.net/v/t39.30808-6/312316633_1324586947946103_3319866972922167684_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=340051&_nc_ohc=MUZ9BD1gS2gAX-QwMiu&_nc_ht=scontent-frx5-1.xx&oh=00_AfAbW6i7m_ajFWWUUkgtkh6F8Eomfc4KjtlR8q2Az5Fm8w&oe=63A9C96D',
+				title: 'Zagrajmy razem w gry planszowe #6'
 			},
 			{
-				date: '22.02.2022',
-				linkFb: '',
+				date: '27.10.2022',
+				linkFb: 'https://www.facebook.com/events/3265430430397507',
+				linkTg: '',
+				img: 'https://scontent-frx5-1.xx.fbcdn.net/v/t39.30808-6/310055521_1302404903497641_8717075860086093235_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=340051&_nc_ohc=DInuVd_mhAoAX8RAtWe&_nc_ht=scontent-frx5-1.xx&oh=00_AfB43ub93DpIhV08_9_KgsHnZRX9mguv2RupP0tgL0WF4w&oe=63A9E675',
+				title: 'Zagrajmy razem w gry planszowe #5'
+			},
+			{
+				date: '13.10.2022',
+				linkFb: 'https://www.facebook.com/events/493621345956493',
+				linkTg: '',
+				img: 'https://scontent-frt3-2.xx.fbcdn.net/v/t39.30808-6/310369722_1302400310164767_7783966992116022696_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=340051&_nc_ohc=BuR_T5CP0C8AX8TyMEX&_nc_ht=scontent-frt3-2.xx&oh=00_AfBorUzC8YHRu1vtKbdulSSJ-CpMEP4JANQsSqJb2jYb4Q&oe=63AB99B4',
+				title: 'Zagrajmy razem w gry planszowe #4'
+			},
+			// upd img
+			{
+				date: '28.09.2022',
+				linkFb: 'https://www.facebook.com/events/609540380725328',
 				linkTg: '',
 				img: '',
-				title: 'Neil Sims'
+				title: 'Zagrajmy razem w gry planszowe #3'
+			},
+			// upd img
+			{
+				date: '13.09.2022',
+				linkFb: 'https://www.facebook.com/events/753954315664277',
+				linkTg: '',
+				img: '',
+				title: 'Zagrajmy razem w gry planszowe #2'
+			},
+			{
+				date: '14.07.2022',
+				linkFb: 'https://www.facebook.com/events/583729213359954',
+				linkTg: '',
+				img: 'https://scontent-frt3-2.xx.fbcdn.net/v/t39.30808-6/292437651_1243596326045166_1477022024164538868_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=340051&_nc_ohc=y9KNdOqsF-YAX8cBxDr&_nc_ht=scontent-frt3-2.xx&oh=00_AfCl97DAnVX5qlBHuV63JHUw1avzN0MXVaZ02hY3X9Lc-Q&oe=63AB3BC7',
+				title: 'Zagrajmy razem w gry planszowe #1'
 			}
 		]
 	},
@@ -177,25 +216,11 @@ const workshopsList = [
 		],
 		harmonogram: [
 			{
-				date: '22.02.2022',
-				linkFb: '',
+				date: '04.10.2022',
+				linkFb: 'https://www.facebook.com/events/1496854624070201',
 				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
-			},
-			{
-				date: '22.02.2022',
-				linkFb: '',
-				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
-			},
-			{
-				date: '22.02.2022',
-				linkFb: '',
-				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
+				img: 'https://scontent-frx5-1.xx.fbcdn.net/v/t39.30808-6/310190464_1302398430164955_8297556960646131003_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=340051&_nc_ohc=WsOJUjk_wOQAX9CcVbG&_nc_ht=scontent-frx5-1.xx&oh=00_AfACecFu5WB0U1H0Sf20fQ0oPjKOAOHgC71NR88a_tz8kw&oe=63AA0152',
+				title: 'Naprawa urządzeń elektrycznych #1'
 			}
 		]
 	},
@@ -225,25 +250,11 @@ const workshopsList = [
 		],
 		harmonogram: [
 			{
-				date: '22.02.2022',
-				linkFb: '',
+				date: '03.11.2022',
+				linkFb: 'https://www.facebook.com/events/1073006690052531',
 				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
-			},
-			{
-				date: '22.02.2022',
-				linkFb: '',
-				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
-			},
-			{
-				date: '22.02.2022',
-				linkFb: '',
-				linkTg: '',
-				img: '',
-				title: 'Neil Sims'
+				img: 'https://scontent-frt3-2.xx.fbcdn.net/v/t39.30808-6/312941585_1324583427946455_7524591385124234362_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=340051&_nc_ohc=Q2iuQx_KkJ0AX8DQGM3&_nc_ht=scontent-frt3-2.xx&oh=00_AfAQ_lE5rpl2hv2Kei9NN4X4s6FwpG8KUdbI8f_5ADuFaA&oe=63AA9C67',
+				title: 'Wyrabiane gier planszowych #1'
 			}
 		]
 	}
