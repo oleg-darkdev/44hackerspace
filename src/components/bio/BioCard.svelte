d1295f3ce4f25c1e0d6d1cb412b6fc9df85e0ee5
DarkDev <darkdev_brand@riseup.net>
2023-01-20 23:28:49 +0200

    fix styles

--- src/components/bio/BioCard.svelte
+++ src/components/bio/BioCard.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <Card  class="m-1">
-  <h5 class="mb-2  text-2xl font-impact text-gray-700 ">{text.heading}</h5>
+  <h3 class="mb-2  text-2xl font-impact text-gray-700 ">{text.heading}</h3>
   <p class="font-normal text-gray-700 leading-tight">
     {text.description}
   </p>
