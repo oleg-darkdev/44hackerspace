393378a3d8ef6d5865259b1d7e0433f58bd3a249
DarkDev <darkdev_brand@riseup.net>
2023-01-20 23:30:12 +0200

    refactoring data structure

---
+++ src/data/boardgamesList.js
@@ -0,0 +1,183 @@
+const boardgamesList = [
+	{
+		title: 'Jenga',
+		description: [
+			'To klasyczna gra Jenga z oryginalnymi drewnianymi klockami! Gra Jenga to oryginalna gra z użyciem drewnianych klocków, którą rodziny kochają od pokoleń.'
+		],
+		img: '/library/jenga.jpg',
+		link: 'https://boardgamegeek.com/boardgame/2452/jenga'
+	},
+	{
+		title: 'Mistakos level',
+		description: [
+			'Mistakos to klasyczna gra zręcznościowa dla dzieci – i nie tylko! Walka o stołki z pewnością pochłonie również starszych graczy. Zapewnij swojej rodzinie wciągająca rozrywkę z nutką zdrowej rywalizacji!'
+		],
+		img: '/library/mistakos.png',
+		link: 'https://boardgamegeek.com/boardgame/292908/mistakos-level'
+	},
+	{
+		title: 'UNO Flip!',
+		description: [
+			'Już od ponad 40 lat UNO bawi i cieszy graczy na całym świecie. To ta sama kultowa gra karciana, ale teraz z nowymi, ekscytującymi elementami – takimi jak dwustronne karty, specjalna karta ODWRÓĆ i ostrzejsze reguły – które podniosą poziom rywalizacji w rozgrywce.'
+		],
+		img: '/library/uno-flip.jpg',
+		link: 'https://boardgamegeek.com/boardgame/271460/uno-flip'
+	},
+	{
+		title: 'Światowy konflikt',
+		description: [
+			'Walka o wpływy, pieniądze i władzę! Akcja gry toczy się w jednym z państw leżącychna granicy dwóch stref wpływów: rosyjskiej i zachodniej.',
+			'Gracze wcielają się w rolęlokalnych polityków starającymi się sięgnąć po władzę. Aby im się to udało, będą musieli skutecznie wykorzystać swoje wpływy w kraju i za granicą.'
+		],
+		img: '/library/world_conflikt.jpg',
+		link: 'https://allegro.pl/oferta/swiatowy-konflikt-imprezowa-gra-planszowa-coup-pl-11097193315'
+	},
+	{
+		title: '6 nimmt',
+		description: [
+			'Celem gry jest zebranie jak najmniejszej ilości kart. Każda karta jest warta minus jeden punkt za każdą głowę byka znajdującą się na niej. Zwycięzcą zostaje osoba, która na końcu gry ma najmniejszą ilość byczych głów.  ',
+			'Proste zasady, ale daje całkiem sporo pokerowych emocji.'
+		],
+		img: '/library/6-bierze.jpg',
+		link: 'https://boardgamegeek.com/boardgame/432/6-nimmt'
+	},
+	{
+		title: 'Carcassonne',
+		description: [
+			'Usiądź z przyjaciółmi przy stole i wspólnie zacznijcie budować z niewielkich żetonów łąki, twierdze, całe miasta i drogi, rywalizując między sobą o przejęcie kontroli nad co bardziej atrakcyjnymi lokacjami.',
+			'Sprytne zagranie może pozbawić rywala możliwości ukończenia ogromnego miasta lub pozwoli Ci przejąć kontrolę nad wielką łąką.'
+		],
+		img: '/library/carcassonne.jpg',
+		link: 'https://boardgamegeek.com/boardgame/822/carcassonne'
+	},
+
+	{
+		title: 'K2',
+		description: [
+			'K2 to rodzinna gra planszowa, w której uczestnicy kierują dwuosobowymi zespołami himalaistów i próbują zdobyć tytułowy szczyt, a następnie bezpiecznie zejść na dół. W tym celu będziemy musieli wybrać odpowiednią drogę, zadbać o kondycję naszych wspinaczy oraz ich zaadaptowanie do zmiennych warunków pogodowych.',
+			''
+		],
+		img: '/library/k2.jpg',
+		link: 'https://boardgamegeek.com/boardgame/73761/k2'
+	},
+	{
+		title: 'ATAK ZOMBIE',
+		description: [
+			'Przygotuj się na atak zombie!',
+			'Nieznany wirus zaatakował ludzi przemieniając większość z nich w zombie. Nie ma czasu na robienie zapasów i długie przygotowania do ucieczki. Chcąc przeżyć, gracze muszą schronić się w jednej z podmiejskich lokacji i odpierać kolejne fale żywych trupów, aż do nadejścia pomocy. Muszą zapewnić sobie jedzenie, lekarstwa i broń. Aby je zdobyć zmuszeni są wychodzić z kryjówek narażając się na spotkanie zombie.'
+		],
+		img: '/library/atak_zombie.jpg',
+		link: 'https://boardgamegeek.com/boardgame/170344/atak-zombie'
+	}
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+	// {
+	//     title: 'Lorem',
+	//     description: [
+	//         '',
+	//         ''
+	//     ],
+	//     img: '/library/',
+	//     link: ''
+	// },
+];
+
+
+export default boardgamesList;
