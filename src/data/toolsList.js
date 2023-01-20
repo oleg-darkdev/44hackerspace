a9c87d01cb3a62b1cb18ef0604af2b116cab8b7c
DarkDev <darkdev_brand@riseup.net>
2023-01-20 23:30:23 +0200

    pseudo data

---
+++ src/data/toolsList.js
@@ -0,0 +1,221 @@
+const boardgamesList = [
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	},
+	{
+		title: 'Lorem',
+		description: ['', ''],
+		img: '/library/',
+		link: ''
+	}
+];
+
+
+export default boardgamesList;
