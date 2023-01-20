c3d35fdc5aad64081b766ed6a57909d8a0106ecc
DarkDev <darkdev_brand@riseup.net>
2023-01-20 19:27:12 +0200

    add full workshops list

---
+++ src/components/AllWorkshopsList.svelte
@@ -0,0 +1,32 @@
+<script>
+    let showFullWorkshopsList = false;
+
+    export let workshopsCounter;
+</script>
+
+
+<div style="" class="p-4 sm:p-6 flex items-center bg-dark m-4 border border-gray-200 shadow-md text-gray-500 rounded-lg flex flex-col max-w-2xl">
+      <dir class="flex items-center items-start flex-row w-full">
+         <span class="max:w-8/12 pt-8 mr-2 text-3xl text-end text-current font-impact">
+          W ciągu historii naszego istnienia zorganizowaliśmy
+         </span>
+         <p class=" flex flex-col items-start ">
+            <span class=" text-7xl -mb-4 text-center text-white font-impact">
+              {workshopsCounter}
+          </span>      
+          <span class="text-3xl text-center text-current font-impact">
+              warsztatów
+          </span>     
+         </p>          
+      </dir>
+      <a name="history">
+      </a>
+                  
+      {#if showFullWorkshopsList}
+        <slot></slot>
+      {/if}
+
+        <button type="button" on:click={() => showFullWorkshopsList = !showFullWorkshopsList} class="lg:w-6/12 md:w-6/12 w-6/12 text-center font-medium focus:ring-4 focus:outline-none inline-flex items-center justify-center px-6 py-3.5 text-base text-dark bg-current hover:bg-white focus:ring-white rounded-lg">
+             {showFullWorkshopsList ? 'Schowaj': 'Czytaj'} całą listę
+        </button>
+  </div>
